cdb17febf34fdaafcc31e0eba454058f0d8d5b1b
nikitapani4 <115365241+nikitapani4@users.noreply.github.com>
2023-03-06 11:38:55 +0300

    Задания 2 семинар

---
+++ cs_task13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+Console.Write("Введите число: ");
+
+string strNumb = Console.ReadLine();
+int len = strNumb.Length;
+int numb = int.Parse(strNumb);
+int a;
+
+if (len < 3)
+{
+    Console.WriteLine("Число не имеет 3 цифры!");
+}
+else
+{
+    if (len == 3)
+    {
+        a = numb % 10;
+        Console.WriteLine($"Третья цифра числа {strNumb} = {a}");
+    }
+    else
+    {
+        numb = numb / 10*(len - 3);
+        a = numb % 10;
+        Console.WriteLine($"Третья цифра числа {strNumb} = {a}");
+    }
+    
+}
---
+++ cs_task15/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.Write("Введите номер дня недели: ");
+
+int numbWeek = int.Parse(Console.ReadLine());
+bool result = (numbWeek == 6) || (numbWeek == 7);
+
+if (result)
+{
+    Console.Write($"День {numbWeek} - выходной день!");
+}
+else
+{
+    Console.Write($"День {numbWeek} - рабочий день!");
+}
