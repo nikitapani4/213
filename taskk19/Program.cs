1c04632916b928af6803c8c0a5ce244d9cfb9ac3
nikitapani4 <115365241+nikitapani4@users.noreply.github.com>
2023-03-09 18:48:26 +0300

    Решение задачи 19

---
+++ taskk19/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Write("Введите число: ");
+string strN = Console.ReadLine();
+int N = int.Parse(strN);
+
+Palindrom(N);
+
+int Multi(int A, int B)
+{
+    int C = 1;
+    int D = 1;
+    while (C < B)
+    {
+        D = D * A;
+        C = C + 1;
+    }
+
+    return D;
+}
+
+void Palindrom(int N)
+{       
+    int numb = 0;
+    int len = strN.Length;
+    int N1 = N;
+
+    while (len > 0)
+    { 
+        numb = numb + Multi(10, len)*(N % 10);
+        N = N / 10;
+        len = len - 1;
+        //Console.WriteLine(numb);
+    }
+
+    if (N1 == numb)
+    {
+        Console.WriteLine($"Число {N1} является палиндромом!");
+    }
+    else
+    {
+        Console.WriteLine($"Число {N1} не является палиндромом!");
+    }
+}
+
