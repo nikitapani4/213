b614d9ea31356e2ab78bab727663349828eb9184
nikita <nikita.panichenko@gmail.com>
2023-03-28 21:23:36 +0300

    Решение задачи 64. Семинар 9

---
+++ task64/Program.cs
@@ -0,0 +1,24 @@
+﻿int N = GetN("Введите значение N: ","Ошибка ввода данных!");
+
+Console.Write($"{N} --> {NaturalNumbersN(N,1)}");
+
+
+
+int GetN(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int numb);
+        if(isCorrect && numb > 0) 
+            return numb;
+        Console.WriteLine(errorMessage);
+    }
+}
+
+string NaturalNumbersN(int num, int one)
+{
+    if(num == one)
+        return num.ToString(); 
+    return num + " " + NaturalNumbersN(num - 1, one);
+}
